f0edcd5d862cd0055dea5d2ce543b4234cbb385d
Roland <metivier.roland@chlorophyt.us>
2021-03-25 06:51:19 -0500

    v0.4.0 ok

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExSemantica.MixProject do
   def project do
     [
       apps_path: "apps",
-      version: "0.3.0",
+      version: "0.4.0",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
