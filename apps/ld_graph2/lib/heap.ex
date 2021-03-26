69dca3c9eda12d7bbc546346555881c0f202bf4a
Roland Metivier <metivier.roland@chlorophyt.us>
2021-03-26 07:31:34 -0500

    add new unique heap

--- apps/ld_graph2/lib/heap.ex
+++ apps/ld_graph2/lib/heap.ex
@@ -1,42 +1,59 @@
 defmodule LdGraph2.Heap do
   @moduledoc """
-  A pairing heap.
+  A pairing heap with unique elements.
   """
-  defstruct [root: 0, children: []]
+  defstruct [par: nil, ch0: nil, ch1: nil]
+
+  @doc """
+  Creates a new heap map, used for storing the main heap.
+  """
+  def new() do
+    %{0 => %LdGraph2.Heap{}}
+  end
 
   @doc """
   Joins two heaps together.
   """
-  def meld(heap1, heap2) when heap1.root > heap2.root do
-    %LdGraph2.Heap{root: heap1.root + 1, children: [heap1 | heap2.children]}
+  def meld(heapmap, heap1i, heap2i) when heap1i < heap2i do
+    heap1 = heapmap |> Map.get(heap1i)
+    heapmap = case heap1 do
+      # Create a new subheap
+      nil -> heapmap |> Map.put(map_size(heapmap), %LdGraph2.Heap{par: heap2i})
+
+      # One for one subheap
+      %LdGraph2.Heap{ch0: nil, ch1: nil} -> %{heapmap | heap1i => %LdGraph2.Heap{heap1 | ch0: heap2i}}
+        
+      # Two for one subheaps
+      %LdGraph2.Heap{ch0: _c0, ch1: nil} -> %{heapmap | heap1i => %LdGraph2.Heap{heap1 | ch1: heap2i}}
+
+      # Create one subheap in place, HACK-y...
+      %LdGraph2.Heap{ch0: ch0, ch1: _c1} -> heapmap |> meld(ch0, map_size(heapmap))
+    end
+    heap2 = heapmap |> Map.get(heap2i)
+    case heap2 do
+      # Nothing changed
+      nil -> heapmap |> Map.put(heap2i, %LdGraph2.Heap{par: 0})
+
+      # Get a subheap then just assign the children from before
+      _h2 -> %{heapmap | heap2i => %LdGraph2.Heap{heap2 | par: heap1i}}
+    end
   end
-  def meld(heap1, heap2) do
-    %LdGraph2.Heap{root: heap2.root + 1, children: [heap2 | heap1.children]}
+
+  def meld(heapmap, heap1i, heap2i) when heap1i > heap2i do
+    heapmap |> meld(heap2i, heap1i)
   end
 
   @doc """
   Inserts a value onto a heap.
   """
-  def insert(heap) do
-    meld(heap, %LdGraph2.Heap{root: heap.root, children: []})
+  def insert(heapmap, heap1i) do
+    heapmap |> meld(heap1i, 0)
   end
 
   @doc """
-  Deletes the maximum (root) value of a heap.
+  Deletes the minimum (root) value of a heap.
   """
-  def delete_max(heap) do
-    merge_pairs(heap.children)
-  end
-
-  def merge_pairs([head1 | []]) do
-    head1
-  end
-
-  def merge_pairs([head1 | [head2 | []]]) do
-    meld(head1, head2)
-  end
-
-  def merge_pairs([head1 | [head2 | tail]]) do
-    meld(meld(head1, head2), merge_pairs(tail))
+  def delete(heapmap, _heap1i) do
+    heapmap # UNIMPLEMENTED
   end
 end
