32b449b76ed7021ff55f65dcd47e3dbd49230be5
Roland Metivier <metivier.roland@chlorophyt.us>
2021-09-08 10:19:13 -0500

    Change ending tag on HTML

--- apps/exsemantica_phx/lib/exsemantica_phx_web/live/components/page_header.ex
+++ apps/exsemantica_phx/lib/exsemantica_phx_web/live/components/page_header.ex
@@ -6,7 +6,7 @@ defmodule ExsemanticaPhxWeb.Components.PageHeader do
     ~L"""
     <%= if @users_mutable do %>
       <button class="float-right text-center m-2 p-3 w-1/6 bg-indigo-100 rounded-full">Log in</button>
-    <%= end %>
+    <% end %>
     """
   end
 end
