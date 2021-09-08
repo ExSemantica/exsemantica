18ab196ee9b3c8b545bc287874062efae709109a
Roland Metivier <metivier.roland@chlorophyt.us>
2021-09-08 09:45:17 -0500

    change the mechanism how users are mutated

--- apps/exsemantica_phx/lib/exsemantica_phx_web/live/components/page_header.ex
+++ apps/exsemantica_phx/lib/exsemantica_phx_web/live/components/page_header.ex
@@ -3,19 +3,10 @@ defmodule ExsemanticaPhxWeb.Components.PageHeader do
 
   @impl true
   def render(assigns) do 
-    case assigns.user_mutability do
-      # TODO: Logins, contracts, yada.
-      :login_open -> ~L"""
-        <button class="float-right text-center m-2 p-3 w-1/6 bg-indigo-100 rounded-full">Log in</button>
-        """
-
-      # TODO: Registration too
-      :registration_open -> ~L"""
-        <button class="float-right text-center m-2 p-3 w-1/6 bg-indigo-100 rounded-full">Log in/Register</button>
-        """
-
-      # Nobody can login or register on this instance. Nothing.
-      _ -> ~L""
-    end
+    ~L"""
+    <%= if @users_mutable do %>
+      <button class="float-right text-center m-2 p-3 w-1/6 bg-indigo-100 rounded-full">Log in</button>
+    <%= end %>
+    """
   end
 end
