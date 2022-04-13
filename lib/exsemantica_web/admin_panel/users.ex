c2f8a6973ff51e0b4232d5e02dd1522785ec2bc4
Roland Metivier <metivier.roland@chlorophyt.us>
2022-04-13 08:52:46 -0500

    IDs of users can now be sorted in admin dashboard

--- lib/exsemantica_web/admin_panel/users.ex
+++ lib/exsemantica_web/admin_panel/users.ex
@@ -13,7 +13,7 @@ defmodule ExsemanticaWeb.AdminPanel.Users do
       id: :users,
       title: "Users",
       columns: [
-        %{field: :id, sortable: :asc},
+        %{field: :id, sortable: :desc},
         %{field: :timestamp},
         %{field: :handle},
         %{field: :privmask}
@@ -21,14 +21,36 @@ defmodule ExsemanticaWeb.AdminPanel.Users do
       row_fetcher: &fetch_rows/2
     )
   end
-  # card(title: "Invite code", value: :persistent_term.get(:exsemantica_invite)),
-  defp fetch_rows(_params, _node) do
-    {:atomic, [reply]} = [%{operation: :dump, table: :users}] |> Exsemnesia.Database.transaction("DUMPING all users in admin panel")
+
+  defp fetch_rows(params, _node) do
+    {:atomic, [reply]} =
+      [%{operation: :dump, table: :users}]
+      |> Exsemnesia.Database.transaction("DUMPING all users in admin panel")
+
     %{operation: :dump, response: entries, table: :users} = reply
 
-    {for {:users, idx, timestamp, handle, privmask} <- entries do
-      <<idx_formatted::128>> = idx
-       %{id: idx_formatted, timestamp: timestamp, handle: handle, privmask: Base.encode16(privmask)}
-     end, length(entries)}
+    unsorted =
+      for {:users, idx, timestamp, handle, privmask} <- entries do
+        <<idx_formatted::128>> = idx
+
+        %{
+          id: idx_formatted,
+          timestamp: timestamp,
+          handle: handle,
+          privmask: Base.encode16(privmask)
+        }
+      end
+
+    if params.sort_dir == :asc do
+      {unsorted
+       |> Enum.sort(fn a, b ->
+         a.id < b.id
+       end), length(entries)}
+    else
+      {unsorted
+       |> Enum.sort(fn a, b ->
+         a.id > b.id
+       end), length(entries)}
+    end
   end
 end
