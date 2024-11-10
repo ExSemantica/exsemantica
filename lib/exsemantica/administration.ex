3398d2369356fd170acf650013556e7a7dd59f11
Roland Metivier <metivier.roland@chlorophyt.us>
2024-11-09 18:39:59 -0600

    rv initialization

--- lib/exsemantica/administration.ex
+++ lib/exsemantica/administration.ex
@@ -2,15 +2,4 @@ defmodule Exsemantica.Administration do
   @moduledoc """
   Administration convenience functions
   """
-  require Logger
-
-  @doc """
-  Remotely initializes the database.
-  """
-  def initialize_database() do
-    :ok = Exsemantica.Repo.config() |> Ecto.Adapters.Postgres.storage_up()
-    Ecto.Migrator.run(Exsemantica.Repo, :up, all: true)
-
-    Logger.info("Done initializing database")
-  end
 end
